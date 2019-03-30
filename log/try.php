a6008e7d631fac3001ac25da1ecf3ffb120438aa
DKrakowczyk <dawid.krakowczyk.1996@gmail.com>
2019-03-30 15:22:39 +0100

    Update try.php

--- log/try.php
+++ log/try.php
@@ -18,8 +18,10 @@ if (isset($_POST['pass'])) {
 
 if (isset($_POST['log']) && isset($_POST['pass'])) {
     try {
+		//While uploading to your custom hosting uncomment this line 
 		//$pdo = new PDO("mysql:host=$serwer;dbname=$database", $user, $password);
-		// For heroku upload
+		//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+		// And delete one below
 		$pdo = new PDO("mysql:host=remotemysql.com;dbname=Rd5FJdRxdo", $user, $password);
         if ($pdo) {
             session_start();
